ff185269e8e7254a6a2a5e40c345a48482ee244b
Froren <jaroslavtalpa@gmail.com>
2024-12-18 13:02:53 +0100

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,22 @@
+plugins {
+    kotlin("jvm") version "2.1.0"
+}
+
+group = "com.froren"
+version = "0.0.1"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation("org.jetbrains.kotlin:kotlin-test")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+kotlin {
+    jvmToolchain(21)
+}
