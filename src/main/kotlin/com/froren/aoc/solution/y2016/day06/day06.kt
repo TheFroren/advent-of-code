f653b3dd7ec984449e1ee695940957a1f52cd578
Froren <jaroslavtalpa@gmail.com>
2024-12-24 18:33:24 +0100

    Year 2016 day 06 solved!

---
+++ src/main/kotlin/com/froren/aoc/solution/y2016/day06/day06.kt
@@ -0,0 +1,52 @@
+package com.froren.aoc.solution.y2016.day06
+
+import com.froren.aoc.common.data.CountingSet
+import com.froren.aoc.common.solve
+
+fun main() {
+    solve(
+        year = 2016,
+        day = 6,
+        part = 1
+    ) {
+        val words = input.toList()
+
+        val counts = List(words.first().length) { CountingSet<Char>() }
+
+        words.forEach { word ->
+            word.forEachIndexed { i, char ->
+                counts[i].add(char)
+            }
+        }
+
+        counts.joinToString("") {
+            it.entries
+                .maxBy { it.value }
+                .key
+                .toString()
+        }
+    }
+
+    solve(
+        year = 2016,
+        day = 6,
+        part = 1
+    ) {
+        val words = input.toList()
+
+        val counts = List(words.first().length) { CountingSet<Char>() }
+
+        words.forEach { word ->
+            word.forEachIndexed { i, char ->
+                counts[i].add(char)
+            }
+        }
+
+        counts.joinToString("") {
+            it.entries
+                .minBy { it.value }
+                .key
+                .toString()
+        }
+    }
+}
